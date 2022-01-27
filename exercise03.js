d6403fc5f01502912de8c1b4a32151b5d50d9ad3
Shalika Ranawaka <76989242+Shali97@users.noreply.github.com>
2022-01-27 11:33:13 +0530

    Add files via upload

---
+++ exercise03.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+// Sample JSON data set1
+const student = [{ id: 1, name: "Shalika", age : "24", city : "Badulla"  },
+            { id: 2, name: "Tishan", age : "26", city : "Gampaha" },
+            { id: 3, name: "Rashi", age : "24", city : "Galle" },
+            { id: 4, name: "Vimukthi", age : "27", city : "Gampaha" },
+            { id: 5, name: "Sulakshi", age : "25", city : "Rathmalana" }]
+
+// Sample JSON data set2
+const courses = [{ id: 1, name: "Shalika", course : "IT", university : "KDU"  },
+             { id: 2, name: "Tishan", course : "Management", university : "Hopkins" },
+             { id: 3, name: "Rashi", course : "SE", university : "KDU" },
+             { id: 4, name: "Vimukthi", course : "HRM", university : "NIBM" },
+             { id: 5, name: "Sulakshi", course : "CS", university : "NSBM" }]
+  
+app.get('/' , (req,res)=>{
+   // This will send the JSON student data to the client.
+    res.send(student); 
+});
+
+app.get('/api/courses' , (req,res)=>{
+    //send course data to the client
+     res.send(courses); 
+});
+
+app.post('/api/courses',(req,res)=>{
+  const course = {
+    id:courses.length +1,
+    name: req.body.name,
+    course: req.body.course
+  };
+  courses.push(course);
+  res.send(course);
+});
+
+app.get('/api/courses/:id' , (req,res)=>{
+     const course = courses.find(c => c.id === parseInt(req.params.id)); 
+     if(!course) res.status(404).json('The course with the given ID was not found'); //404 error
+     res.json(course);
+});
+  
+// Server setup
+app.listen(8000, ()=>{
+    console.log("server running");
+});

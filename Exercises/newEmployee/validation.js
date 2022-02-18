e2844cd49ec8e1d394e78d92a6415b99db938de1
Shalika Ranawaka <76989242+Shali97@users.noreply.github.com>
2022-02-18 08:56:55 +0530

    Create validation.js

---
+++ Exercises/newEmployee/validation.js
@@ -0,0 +1,10 @@
+const Joi = require('joi');
+
+exports.employee = Joi.object({
+    name: Joi.string().min(5).max(50).required(),
+    address: Joi.string().max(200).min(20).required(),
+    dob: Joi.string().required(),
+    email: Joi.string().min(5).max(255).email().required(),
+    contact_no: Joi.string().min(10).max(15).required()
+})
+
